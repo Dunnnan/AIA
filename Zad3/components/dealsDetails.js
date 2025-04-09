40723f9fe910f7b1af57963cd8e96615d8c616c7
Dunnnan <777dunny@gmail.com>
2025-04-09 12:38:04 +0200

    Dokończenie Zad3 - poprawka w html

--- Zad3/components/dealsDetails.js
+++ Zad3/components/dealsDetails.js
@@ -17,14 +17,14 @@ export async function loadDealsDetails(deal) {
             <img src="${deal.thumb}">
 
             <p class="title"><strong>Title:</strong> ${deal.title}</p>
-
+            <br>
             <p class="salePrice"><strong>Price:</strong> ${deal.salePrice}$</p>
             <p class="normalPrice"><strong>Normal price:</strong> ${deal.normalPrice}$</p>
             <p class="savings"><strong>Discount:</strong> -${Math.trunc(deal.savings)}%</p>
-
+            <br>
             <p class="metacriticScore"><strong>Metacritic score:</strong> ${deal.metacriticScore}</p>
             <p class="dealRating"><strong>Deal rating:</strong> ${deal.dealRating}</p>
-
+            <br>
             <p class="storeName"><strong>Store :</strong> ${storeName}</p>
             <img src="https://www.cheapshark.com${storeLogo}" "class="storeName"></p>
             
